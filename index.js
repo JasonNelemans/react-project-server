dcc673b8818eb66cc857ed627276010034cc7490
shingymen <kahshing90@hotmail.com>
2020-02-25 16:34:43 +0100

    server

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const express = require('')
+//import express
+const express = require("express");
+
+//express server
+const app = express();
+
+//handler function
+function onRequest() {
+  console.log("after request");
+}
+
+//port number
+const port = 3000;
+
+//confirmation function
+function onListen() {
+  console.log(`Listening on :${port}`);
+}
+
+//port starting listening
+app.listen(port, onListen);
